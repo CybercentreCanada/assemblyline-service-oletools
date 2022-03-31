7e9b13e66136173544a09f3bdd4a621181179ce6
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2022-03-31 12:48:53 -0400

    Some strings that VBA_Parser considers suspicious are too common to be scored

--- oletools_/oletools_.py
+++ oletools_/oletools_.py
@@ -1241,8 +1241,7 @@ class Oletools(ServiceBase):
                     assert autoexecution.heuristic
                     autoexecution.heuristic.add_signature_id(keyword)
             if suspicious:
-                signatures = {keyword: 1 for keyword in suspicious
-                              if keyword not in ('hex strings', 'base64 strings')}
+                signatures = {keyword: 1 for keyword in suspicious}
                 heuristic = Heuristic(30, signatures=signatures) if signatures else None
                 macro_section.add_subsection(ResultSection("Suspicious strings or functions",
                                                            heuristic=heuristic,
