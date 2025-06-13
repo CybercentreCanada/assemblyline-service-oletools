73e31d2975f60cc5b1d1a81f6f406766241e0a41
cccs-jh <63320703+cccs-jh@users.noreply.github.com>
2025-06-13 15:06:51 +0000

    Move signature description functions outside of Oletools
    The function format_certificate, describe_attribute, and
    describe_signer_info don't rely on any Oletools specific data.
    Moving them as functions to a new module oletools_.signatures
    to avoid cluttering the oletools_.oletools_ module.

--- oletools_/oletools_.py
+++ oletools_/oletools_.py
@@ -42,21 +42,19 @@ from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.result import BODY_FORMAT, Heuristic, Result, ResultKeyValueSection, ResultSection
 from assemblyline_v4_service.common.task import MaxExtractedExceeded
 from lxml import etree
-from signify.authenticode import AuthenticodeSignedData, AuthenticodeSignerInfo, RawCertificateFile, RFC3161SignedData
-from signify.pkcs7 import SignedData, SignerInfo
+from signify.authenticode import AuthenticodeSignedData, RawCertificateFile
 
 from oletools import mraptor, msodde, oleid, oleobj, olevba, rtfobj
 from oletools.common import clsid
 from oletools.thirdparty.xxxswf import xxxswf
 from oletools_.cleaver import OLEDeepParser
+from oletools_.signatures import describe_signed_data
 from oletools_.stream_parser import PowerPointDoc
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, Mapping
 
-    from asn1crypto.core import Asn1Value
     from assemblyline_v4_service.common.request import ServiceRequest
-    from signify.x509 import Certificate
 
 # Type definition for tags
 Tags = dict[str, list[str]]
@@ -89,16 +87,6 @@ def is_safelisted(
     return tag_contains_match(tag, safelist_matches.get(tag_type, [])) or regex_matches_tag(
         tag, safelist_regexes.get(tag_type, [])
     )
-
-
-def format_certificate(cert: Certificate) -> dict[str, str]:
-    return {
-        "subject": cert.subject.dn,
-        "issuer": cert.issuer.dn,
-        "serial": str(cert.serial_number),
-        "valid_from": str(cert.valid_from),
-        "valid_to": str(cert.valid_to),
-    }
 
 
 class Oletools(ServiceBase):
@@ -866,105 +854,6 @@ class Oletools(ServiceBase):
 
         return streams_section
 
-    def describe_attribute(self, name: str, values: list[Asn1Value]) -> dict[str, Any]:
-        if name in (
-            "microsoft_time_stamp_token",
-            "microsoft_spc_sp_opus_info",
-            "counter_signature",
-        ):
-            return {name: "(elided)"}
-        if name == "message_digest":
-            return {name: values[0].native.hex()}
-        if len(values) == 1:
-            return {name: values[0].native}
-        return {name: [value.native for value in values]}
-
-    def describe_signer_info(self, signer_info: SignerInfo) -> dict[str, Any]:
-        result: dict[str, Any] = {
-            "issuer": signer_info.issuer.dn,
-            "serial": str(signer_info.serial_number),
-            "digest_algorithm": signer_info.digest_algorithm.__name__,
-            "digest_encryption_algorithm": signer_info.digest_encryption_algorithm,
-            "encrypted_digest": signer_info.encrypted_digest.hex(),
-        }
-
-        if signer_info.authenticated_attributes:
-            result["authenticated_attributes"] = [
-                self.describe_attribute(*attribute) for attribute in signer_info.authenticated_attributes.items()
-            ]
-        if signer_info.unauthenticated_attributes:
-            result["unauthenticated_attributes"] = [
-                self.describe_attribute(*attribute) for attribute in signer_info.unauthenticated_attributes.items()
-            ]
-
-        if isinstance(signer_info, AuthenticodeSignerInfo):
-            result["opus_info"] = {
-                "program_name": signer_info.program_name,
-                "more_info": signer_info.more_info,
-                "publisher_info": signer_info.publisher_info,
-            }
-
-        if signer_info.countersigner:
-            # SignerInfo.countersigner's type is a lie,
-            # Subclass AuthenticodeSignerInfo's countersigner can be RFC3161SignedData
-            # https://github.com/ralphje/signify/blob/42975a08d8738a9d107fda2b239288d0948f87e0/signify/authenticode/structures.py#L813
-            countersigner: SignerInfo | RFC3161SignedData = signer_info.countersigner
-            if isinstance(countersigner, SignerInfo):
-                result["countersigner"] = {
-                    "signing_time": getattr(signer_info.countersigner, "signing_time", None),
-                    "info": self.describe_signer_info(signer_info.countersigner),
-                }
-            if isinstance(signer_info.countersigner, RFC3161SignedData):
-                result["countersigner_nested_rfc3161"] = self.describe_signed_data(signer_info.countersigner)
-
-        return result
-
-    def describe_signed_data(self, signed_data: SignedData) -> dict[str, Any]:
-        result = {
-            "certificates": [format_certificate(cert) for cert in signed_data.certificates],
-            "signer": self.describe_signer_info(signed_data.signer_info),
-            "digest_algorithm": signed_data.digest_algorithm.__name__,
-            "content_type": signed_data.content_type,
-        }
-
-        if isinstance(signed_data, AuthenticodeSignedData) and signed_data.indirect_data:
-            indirect: dict[str, Any] = {
-                "digest_algorithm": signed_data.indirect_data.digest_algorithm.__name__,
-                "digest": signed_data.indirect_data.digest.hex(),
-                "content_type": signed_data.indirect_data.content_type,
-            }
-            pe_image_data = signed_data.indirect_data.content
-            if pe_image_data:
-                pe_data: dict[str, Any] = {
-                    "flags": pe_image_data.flags,
-                    "file_link_type": pe_image_data.file_link_type,
-                }
-                if pe_image_data.file_link_type == "moniker":
-                    pe_data["class_id"] = pe_image_data.class_id
-                    pe_data["content_types"] = pe_image_data.content_types
-                else:
-                    pe_data["publisher"] = pe_image_data.publisher
-                indirect["pe_image_data"] = pe_data
-            result["indirect_data"] = indirect
-
-        if isinstance(signed_data, RFC3161SignedData) and signed_data.tst_info:
-            result["tst_info"] = {
-                "hash_algorithm": signed_data.tst_info.hash_algorithm.__name__,
-                "digest": signed_data.tst_info.message_digest.hex(),
-                "serial_number": str(signed_data.tst_info.serial_number),
-                "signing_time": str(signed_data.tst_info.signing_time),
-                "signing_time_accuracy": str(signed_data.tst_info.signing_time_accuracy),
-                "signing_authority": str(signed_data.tst_info.signing_authority),
-            }
-
-        if isinstance(signed_data, AuthenticodeSignedData):
-            verify_result, e = signed_data.explain_verify()
-            result["verify_result"] = str(verify_result)
-            if e:
-                result["verify_error"] = str(e)
-
-        return result
-
     def _format_signer(self, signed_datas: list[dict[str, Any]]) -> tuple[dict[str, Any], dict[str, Any]]:
         # The following logic finds the signer certificate
         # by comparing the certificate's issuer with the signeddata's signer info.
@@ -1017,7 +906,7 @@ class Oletools(ServiceBase):
         """
         try:
             sig = RawCertificateFile(BytesIO(signature))
-            signed_datas = [self.describe_signed_data(signed_data) for signed_data in sig.signed_datas]
+            signed_datas = [describe_signed_data(signed_data) for signed_data in sig.signed_datas]
             verify_result, verify_error = sig.explain_verify()
             tags, formatted_signature = self._format_signer(signed_datas)
 
